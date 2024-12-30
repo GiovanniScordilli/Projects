dd0975cc9a09236e78293501684f9e10752fe03b
Giovanni Scordilli <169074801+GiovanniScordilli@users.noreply.github.com>
2024-12-30 21:57:05 +0100

    Update stack.h

--- C-Containers/stack/stack.h
+++ C-Containers/stack/stack.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdio.h>
 
 struct node
 {
@@ -19,7 +18,5 @@ bool is_empty(stack *node);
 stack* create_node(double node_value);
 void push(stack **node, double element_value);
 void pop(stack **node);
-
-void print_stack(stack *node);
 
 #endif
