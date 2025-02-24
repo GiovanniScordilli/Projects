660513b4289c7c9b3fd48c6c0a2ffd29546215f6
Giovanni Scordilli <169074801+GiovanniScordilli@users.noreply.github.com>
2025-02-24 17:42:51 +0100

    Update main.cpp

--- OpenCL/OpenCL-Setup/main.cpp
+++ OpenCL/OpenCL-Setup/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * Author: Scordilli Giovanni
+ * Project: base prgoram to run an OpenCL application, you can 
+            choose every paramater to istantiate your app.
+ * Date: 06/01/2025
+ */
+
 #include "setup.h"
 
 int main(int argc, char** argv)
